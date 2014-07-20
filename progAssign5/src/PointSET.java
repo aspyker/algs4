54ea2d5967576f386c91bd3609e4bb8945de463d
aspyker <awspyker@gmail.com>
2014-07-20 12:15:44 -0400

    28 precent, just implemented PointSet to 100 percent so far

--- progAssign5/src/PointSET.java
+++ progAssign5/src/PointSET.java
@@ -1,40 +1,91 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class PointSET {
+    private TreeSet<Point2D> set;
+
+    /**
+     * construct an empty set of points
+     */
     public PointSET() {
-        // construct an empty set of points
+        set = new TreeSet<Point2D>();
     }
     
+    /**
+     * 
+     * @return is the set empty?
+     */
     public boolean isEmpty() {
-        // is the set empty?
-        throw new UnsupportedOperationException();
+        return set.isEmpty();
     }
-    
+
+    /**
+     * 
+     * @return number of points in the set
+     */
     public int size() {
-        // number of points in the set
-        throw new UnsupportedOperationException();
+        return set.size();
     }
     
+    /**
+     * add the point p to the set (if it is not already in the set)
+     * @param p
+     */
     public void insert(Point2D p) {
-        // add the point p to the set (if it is not already in the set)
-        throw new UnsupportedOperationException();
+        set.add(p);
     }
     
+    /**
+     * does the set contain the point p?
+     * @param p
+     * @return
+     */
     public boolean contains(Point2D p) {
-        // does the set contain the point p?
-        throw new UnsupportedOperationException();
+        return set.contains(p);
     }
     
+    /**
+     * draw all of the points to standard draw
+     */
     public void draw() {
-        // draw all of the points to standard draw
-        throw new UnsupportedOperationException();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.01);
+        for (Point2D point : set) {
+            StdDraw.point(point.x(), point.y());
+        }
     }
     
+    /**
+     * all points in the set that are inside the rectangle
+     * @param rect
+     * @return
+     */
     public Iterable<Point2D> range(RectHV rect) {
-        // all points in the set that are inside the rectangle
-        throw new UnsupportedOperationException();
+        ArrayList<Point2D> list = new ArrayList<Point2D>();
+        for (Point2D point : set) {
+            if  (rect.contains(point)) {
+                list.add(point);
+            }
+        }
+        return list;
     }
-    
+
+    /**
+     * a nearest neighbor in the set to p; null if set is empty
+     * @param p
+     * @return
+     */
     public Point2D nearest(Point2D p) {
-        // a nearest neighbor in the set to p; null if set is empty
-        throw new UnsupportedOperationException();
+        double nearestDistance = Double.MAX_VALUE;
+        Point2D nearestPoint = null;
+        
+        for (Point2D point : set) {
+            double newDistance = p.distanceTo(point);
+            if (newDistance < nearestDistance) {
+                nearestPoint = point;
+                nearestDistance = newDistance;
+            }
+        }
+        return nearestPoint;
     }
 }
--- progAssign5/src/TestPointSet.java
+++ progAssign5/src/TestPointSet.java
@@ -1,3 +1,39 @@
-public class TestPointSet {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPointSet {
+    PointSET ps;
+    
+    @Before
+    public void setup() {
+        ps = new PointSET();
+        ps.insert(new Point2D(0.5, 0.5));
+        ps.insert(new Point2D(0.25, 0.25));
+        ps.insert(new Point2D(0.76, 0.76));
+    }
+    
+    @Test
+    public void testPointSETDraw() throws Exception {
+        ps.draw();
+        StdDraw.show();
+        Thread.sleep(5000);
+    }
+    
+    @Test
+    public void testPointSETNearest() {
+        Point2D nearest = ps.nearest(new Point2D(0.4, 0.4));
+        assertTrue(nearest.equals(new Point2D(0.5, 0.5)));
+    }
+    
+    @Test
+    public void testPointSETRange() {
+        RectHV rect = new RectHV(0.20, 0.20, 0.6, 0.6);
+        for (Point2D point : ps.range(rect)) {
+            assertTrue(
+                point.equals(new Point2D(0.5, 0.5)) ||
+                point.equals(new Point2D(0.25, 0.25)));
+        }
+    }
 }
