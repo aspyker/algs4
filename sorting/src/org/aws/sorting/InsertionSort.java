d97decd17f8de5ed4924caf32fc258100169a5ed
aspyker <awspyker@gmail.com>
2014-07-20 11:20:13 -0400

    remove printlns

--- sorting/src/org/aws/sorting/InsertionSort.java
+++ sorting/src/org/aws/sorting/InsertionSort.java
@@ -2,12 +2,12 @@ package org.aws.sorting;
 
 public class InsertionSort<Item extends Comparable<Item>> {
     public void sort(Item[] array) {
-        int arrayAccesses = 0;
+//        int arrayAccesses = 0;
         for (int ii = 1 ; ii < array.length; ii++) {
             for (int jj = ii; jj > 0; jj--) {
                 Item jjItem = array[jj];
                 Item leftOfJJItem = array[jj - 1];
-                arrayAccesses+=2;
+//                arrayAccesses+=2;
                 if (jjItem.compareTo(leftOfJJItem) < 0) {
                     swap(array, jj, jj - 1);
                 }
@@ -16,7 +16,7 @@ public class InsertionSort<Item extends Comparable<Item>> {
                 }
             }
         }
-        System.out.println("array accesses = " + arrayAccesses);
+//        System.out.println("array accesses = " + arrayAccesses);
     }
     
     private void swap(Item[] array, int first, int second) {
--- sorting/src/org/aws/sorting/SelectionSort.java
+++ sorting/src/org/aws/sorting/SelectionSort.java
@@ -2,14 +2,14 @@ package org.aws.sorting;
 
 public class SelectionSort<Item extends Comparable<Item>> {
     public void sort(Item[] array) {
-        int arrayAccesses = 0;
+//        int arrayAccesses = 0;
         for (int ii = 0 ; ii < array.length - 1; ii++) {
             Item smallest = array[ii];
-            arrayAccesses++;
+//            arrayAccesses++;
             int currentSmallestIndex = ii;
             for (int jj = ii + 1; jj < array.length; jj++) {
                 Item current = array[jj];
-                arrayAccesses++;
+//                arrayAccesses++;
                 if (current.compareTo(smallest) < 0) {
                     smallest = current;
                     currentSmallestIndex = jj;
@@ -20,7 +20,7 @@ public class SelectionSort<Item extends Comparable<Item>> {
                 //arrayAccesses = arrayAccesses + 2;
             }
         }
-        System.out.println("array accesses = " + arrayAccesses);
+//        System.out.println("array accesses = " + arrayAccesses);
     }
     
     private void swap(Item[] array, int first, int second) {
